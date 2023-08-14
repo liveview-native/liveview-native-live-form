f2691171974466002a0c56e95611c304bcfc2078
May Matyi <may@matyi.net>
2023-08-14 14:01:18 -0700

    Use phx-change-support branch

--- Package.swift
+++ Package.swift
@@ -18,7 +18,7 @@ let package = Package(
     ],
     dependencies: [
         // Dependencies declare other packages that this package depends on.
-        .package(url: "https://github.com/liveviewnative/liveview-client-swiftui.git", branch: "main"),
+        .package(url: "https://github.com/liveviewnative/liveview-client-swiftui.git", branch: "phx-change-support"),
     ],
     targets: [
         // Targets are the basic building blocks of a package. A target can define a module or a test suite.

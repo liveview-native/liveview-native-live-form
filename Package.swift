4c56eb59f02e75547a46285733e7bdfd76f77070
Carson Katri <Carson.katri@gmail.com>
2024-01-16 10:59:40 -0500

    Upgrade client to 0.2.0

--- Package.swift
+++ Package.swift
@@ -1,4 +1,4 @@
-// swift-tools-version: 5.7
+// swift-tools-version: 5.9
 // The swift-tools-version declares the minimum version of Swift required to build this package.
 
 import PackageDescription
@@ -18,7 +18,7 @@ let package = Package(
     ],
     dependencies: [
         // Dependencies declare other packages that this package depends on.
-        .package(url: "https://github.com/liveviewnative/liveview-client-swiftui.git", branch: "main"),
+        .package(url: "https://github.com/liveviewnative/liveview-client-swiftui.git", from: "0.2.0"),
     ],
     targets: [
         // Targets are the basic building blocks of a package. A target can define a module or a test suite.

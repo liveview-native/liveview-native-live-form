55c1d22f7dffd38ba944a59b352f39d14b369c77
Carson Katri <Carson.katri@gmail.com>
2024-03-26 09:42:56 -0400

    Depend on `main` branch

--- Package.swift
+++ Package.swift
@@ -18,7 +18,7 @@ let package = Package(
     ],
     dependencies: [
         // Dependencies declare other packages that this package depends on.
-        .package(url: "https://github.com/liveviewnative/liveview-client-swiftui.git", from: "0.2.0"),
+        .package(url: "https://github.com/liveviewnative/liveview-client-swiftui.git", branch: "main"),
     ],
     targets: [
         // Targets are the basic building blocks of a package. A target can define a module or a test suite.

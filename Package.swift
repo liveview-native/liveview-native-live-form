22f5eced128c86eb59e5df1da23176f83b7f809d
Brian Cardarella <bcardarella@gmail.com>
2024-04-23 18:21:36 -0400

    Fix dependency

--- Package.swift
+++ Package.swift
@@ -18,7 +18,7 @@ let package = Package(
     ],
     dependencies: [
         // Dependencies declare other packages that this package depends on.
-        .package(url: "https://github.com/liveview-native/liveview-client-swiftui.git", branch: "main"),
+        .package(url: "https://github.com/liveview-native/liveview-client-swiftui.git", from: "0.3.0-rc.1"),
     ],
     targets: [
         // Targets are the basic building blocks of a package. A target can define a module or a test suite.
